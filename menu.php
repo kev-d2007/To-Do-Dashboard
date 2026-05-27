959006494e3f7889afc1ed909e53018dfddd59b9
kev-d2007 <kevindbetting@gmail.com>
2026-05-27 11:47:40 +0200

    basic knoppen toegevoegd

---
+++ menu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <img src="img/logo.png" alt="Logo" class="logo">
+    <button onclick="location.href='index.php'">Dashboard</button>
+    <button onclick="location.href='stats.php'">Statistieken</button>
+    <button onclick="location.href='settings.php'">Instellingen</button>
+    <a>Categorieën</a>
+    <button onclick="location.href='logout.php'">Uitloggen</button>
+</body>
+</html>
