0c4399d88f0b2722694f7040fbf97c1f73842e40
kev-d2007 <kevindbetting@gmail.com>
2026-05-27 12:15:24 +0200

    new dashboard

---
+++ dashboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Mijn taken</h1>
+    <form action="toevoegen.php" method="post">
+        <input type="text" name="task" placeholder="Nieuwe taak toevoegen">
+        <button type="submit">Toevoegen</button>
+    </form>
+</body>
+</html>
--- menu.php
+++ menu.php
@@ -3,14 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Home</title>
 </head>
 <body>
-    <img src="img/logo.png" alt="Logo" class="logo">
-    <button onclick="location.href='index.php'">Dashboard</button>
-    <button onclick="location.href='stats.php'">Statistieken</button>
-    <button onclick="location.href='settings.php'">Instellingen</button>
+    <script>
+        function get_page(page) {
+            <?php
+            $page = $_GET['page'] ?? 'dashboard.php';
+            include_once $page;
+            ?>
+            console.log(page + " is aan het laden...");
+        }
+    </script>
+    <img src="img/logo.png" alt="Logo" class="logo" id="main-logo">
+    <button onclick="get_page('dashboard.php')">Dashboard</button>
+    <button onclick="get_page('stats.php')">Statistieken</button>
+    <button onclick="get_page('settings.php')">Instellingen</button>
     <a>Categorieën</a>
-    <button onclick="location.href='logout.php'">Uitloggen</button>
+    <button onclick="get_page('logout.php')">Uitloggen</button>
 </body>
 </html>
